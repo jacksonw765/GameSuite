1fd932904fcb56e09fbc5bcf581a9f6c2d119579
Jackson Welch <jacksonw765@gmail.com>
2019-10-01 16:43:34 -0400

    added frontend nav bar

--- gamesuite/urls.py
+++ gamesuite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from common.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_main)
 ]
