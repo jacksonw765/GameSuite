2fe36ebbfd37413a99d6bc501a31b05a75164e0b
Jackson Welch <jacksonw765@gmail.com>
2020-01-10 20:24:05 -0500

    added sign in auth for main page

---
+++ common/static/js/main.js
@@ -0,0 +1,3 @@
+function usernamePassSignIn() {
+    $('#myModal').show();
+}
--- leaderboard/static/js/leaderboard.js
+++ leaderboard/static/js/leaderboard.js
@@ -1,5 +1,8 @@
 window.onload = function () {
     loadLeaderboard();
+    $.getJSON('https://json.geoiplookup.io', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
 };
 
 function loadLeaderboard() {
