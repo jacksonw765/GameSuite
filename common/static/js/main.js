4717348e88f6d91d086fa37d24bb3bd4cb372edb
Jackson Welch <jacksonw765@gmail.com>
2020-01-12 14:56:29 -0500

    added more stuff for user auth

--- common/static/js/main.js
+++ common/static/js/main.js
@@ -25,14 +25,42 @@ function showSignIn() {
 }
 
 function signInUser() {
-    console.log('sign in user');
     var email = $('#sign_in_id_email').val();
     var password = $('#sign_in_id_password').val();
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorMessage)
-    });
+    if (emailValidation(email)) {
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            showAlertSignIn(errorMessage, 'alert-danger');
+        });
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user)
+                location.reload();
+        });
+    } else {
+        showAlertSignIn("Email is invalid", 'alert-danger');
+    }
+}
+
+function emailValidation(email) {
+    let pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return email.match(pattern);
+}
+
+function showAlertCreate(message, alert) {
+    console.log('alert');
+    $('#create-account-modal-content').append('<div style="margin: 5px" id="alertdiv" class="alert" ' + alert + '"><a class="close" data-dismiss="alert">X</a><span style="font-size:14px">' + message + '</span></div>');
+    setTimeout(function () {
+        $("#alertdiv").remove();
+    }, 5000);
+}
+
+function showAlertSignIn(message) {
+    console.log('alert');
+    $('#modal-sign-in-body').append('<div id="alertdiv" style="margin: 10px" class="alert alert-danger"><a class="close" data-dismiss="alert">X</a><span style="font-size:14px">' + message + '</span></div>');
+    setTimeout(function () {
+        $("#alertdiv").remove();
+    }, 5000);
 }
 
 function createNewUser() {
@@ -42,6 +70,7 @@ function createNewUser() {
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.message;
+        $('#create-account-danger-alert').alert();
         console.log(errorMessage)
     });
 }
