435d18301c1cb0e570183a3f35a38dc66c3933df
Jackson Welch <jacksonw765@gmail.com>
2020-01-13 16:19:18 -0500

    fixed sign in main page

--- common/static/js/main.js
+++ common/static/js/main.js
@@ -25,20 +25,20 @@ function showSignIn() {
 }
 
 function signInUser() {
-    var email = $('#sign_in_id_email').val();
-    var password = $('#sign_in_id_password').val();
+    let email = $('#sign_in_id_email').val();
+    let password = $('#sign_in_id_password').val();
     if (emailValidation(email)) {
         firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
             var errorCode = error.code;
             var errorMessage = error.message;
-            showAlertSignIn(errorMessage, 'alert-danger');
+            showAlertSignIn(errorMessage);
         });
         firebase.auth().onAuthStateChanged(function (user) {
             if (user)
                 location.reload();
         });
     } else {
-        showAlertSignIn("Email is invalid", 'alert-danger');
+        showAlertSignIn("Email is invalid");
     }
 }
 
@@ -47,30 +47,40 @@ function emailValidation(email) {
     return email.match(pattern);
 }
 
-function showAlertCreate(message, alert) {
-    console.log('alert');
-    $('#create-account-modal-content').append('<div style="margin: 5px" id="alertdiv" class="alert" ' + alert + '"><a class="close" data-dismiss="alert">X</a><span style="font-size:14px">' + message + '</span></div>');
+function showAlertCreate(message) {
+    $('#create-account-modal-content').append('<div id="alertdiv" style="margin: 10px" class="alert alert-danger"><a class="close" data-dismiss="alert">X</a><span style="font-size:16px">' + message + '</span></div>');
     setTimeout(function () {
         $("#alertdiv").remove();
     }, 5000);
 }
 
 function showAlertSignIn(message) {
-    console.log('alert');
-    $('#modal-sign-in-body').append('<div id="alertdiv" style="margin: 10px" class="alert alert-danger"><a class="close" data-dismiss="alert">X</a><span style="font-size:14px">' + message + '</span></div>');
+    $('#modal-sign-in-body').append('<div id="alertdiv" style="margin: 10px" class="alert alert-danger"><a class="close" data-dismiss="alert">X</a><span style="font-size:16px">' + message + '</span></div>');
     setTimeout(function () {
         $("#alertdiv").remove();
     }, 5000);
 }
 
 function createNewUser() {
-    console.log('create user');
-    var email = $('#create_id_email').val();
-    var password = $('#create_id_password').val();
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        $('#create-account-danger-alert').alert();
-        console.log(errorMessage)
-    });
+    let username = $('#create_id_username').val();
+    let email = $('#create_id_email').val();
+    let password = $('#create_id_password').val();
+    if (emailValidation(email)) {
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            showAlertCreate(errorMessage);
+            console.log(errorMessage);
+        });
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user)
+                location.reload();
+        });
+    } else {
+        showAlertCreate('Email is Invalid');
+    }
+}
+
+function checkUsernameInUse() {
+
 }
