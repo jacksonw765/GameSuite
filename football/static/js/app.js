f55e719ed0e536b81230b1dcb34b809b2287b71e
colinbaca <bacacr@mail.uc.edu>
2020-02-04 19:03:18 -0500

    Added player Sprite and more game functionality

--- football/static/js/app.js
+++ football/static/js/app.js
@@ -35,13 +35,15 @@ var cam;
 
 var game = new Phaser.Game(config);
 
-function preload() {
-    this.load.image('background', field);
-    this.load.spritesheet('sprite', sprite, {frameWidth: 41.3, frameHeight: 36});
-    this.load.image('enemy', enemy);
-}
+function preload ()
+{
+    this.load.image('background', '/assets/field.png');
+    this.load.spritesheet('sprite', 'assets/footballSprite.png', { frameWidth: 66, frameHeight: 66 });
+    this.load.image('enemy', 'assets/UC_Sprite.png');
+};
 
-function create() {
+function create ()
+{
     // Game Background
     background = this.add.image(250, 200, 'background').setScale(1.75);
     background.x = 0;
@@ -66,33 +68,82 @@ function create() {
     );
 
     //  Produce Enemies
-    enemies = this.physics.add.group({
+    enemies1 = this.physics.add.group({
         key: 'enemy',
         repeat: 10,
-        setXY: {x: -475, y: -500, stepX: 95},
+        runChildUpdate: true,
+        setXY: { x: -475, y: -200, stepX: 95 },
+        collideWorldBounds: true
+    });
+
+    enemies2 = this.physics.add.group({
+        key: 'enemy',
+        repeat: 10,
+        runChildUpdate: true,
+        setXY: { x: -475, y: -500, stepX: 95 },
+        collideWorldBounds: true
+    });
+
+    enemies3 = this.physics.add.group({
+        key: 'enemy',
+        repeat: 10,
+        runChildUpdate: true,
+        setXY: { x: -475, y: -800, stepX: 95 },
+        collideWorldBounds: true
+    });
+
+    enemies4 = this.physics.add.group({
+        key: 'enemy',
+        repeat: 10,
+        runChildUpdate: true,
+        setXY: { x: -475, y: -1000, stepX: 95 },
         collideWorldBounds: true
     });
 
     //  Give each enemy a slightly different speed
-    enemies.children.iterate(function (child) {
+    enemies1.children.iterate(function (child) {
         child.setVelocityX(Phaser.Math.FloatBetween(-50, 50));
-        child.setVelocityY(Phaser.Math.FloatBetween(25, 100));
+        child.setVelocityY(Phaser.Math.FloatBetween(70, 100));
+    });
+
+    enemies2.children.iterate(function (child) {
+        child.setVelocityX(Phaser.Math.FloatBetween(-50, 50));
+        child.setVelocityY(Phaser.Math.FloatBetween(60, 90));
+    });
+
+    enemies3.children.iterate(function (child) {
+        child.setVelocityX(Phaser.Math.FloatBetween(-50, 50));
+        child.setVelocityY(Phaser.Math.FloatBetween(50, 80));
+    });
+
+    enemies4.children.iterate(function (child) {
+        child.setVelocityX(Phaser.Math.FloatBetween(-50, 50));
+        child.setVelocityY(Phaser.Math.FloatBetween(40, 70));
     });
 
     // Enemies Follow Player
-    //this.enemies.startFollow(player, true);
+    // enemies.startFollow(player);
 
     // Enemies Collide
-    this.physics.add.collider(enemies, enemies);
-    this.physics.add.collider(enemies, sideline);
+    // this.physics.add.collider(enemies, enemies);
+    this.physics.add.collider(enemies1, sideline);
+    this.physics.add.collider(enemies2, sideline);
+    this.physics.add.collider(enemies3, sideline);
+    this.physics.add.collider(enemies4, sideline);
+
 
     // Player Collision is ON
     player.setCollideWorldBounds(true);
 
+    // Setting score text to a fixed position in the top-left corner
+    scoreText = this.add.text(0, 0, "Score: 0").setScrollFactor(0).setFontSize(40).setColor('#000000');
+
+
     // Set Camera to follow Player
     this.cameras.main.startFollow(player, true);
 
-    if (this.cameras.main.deadzone) {
+    if (this.cameras.main.deadzone)
+    {
         graphics = this.add.graphics().setScrollFactor(0);
         graphics.lineStyle(2, 0x00ff00, 1);
         graphics.strokeRect(200, 200, this.cameras.main.deadzone.width, this.cameras.main.deadzone.height);
@@ -101,50 +152,53 @@ function create() {
     // Player animations
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('sprite', {start: 9, end: 11}),
-        frameRate: 20,
+        frames: this.anims.generateFrameNumbers('sprite', { start: 4, end: 7 }),
+        frameRate: 15,
         repeat: -1
     });
 
     this.anims.create({
         key: 'forward',
-        frames: this.anims.generateFrameNumbers('sprite', {start: 6, end: 8}),
-        frameRate: 20,
+        frames: this.anims.generateFrameNumbers('sprite', { start: 12, end: 15 }),
+        frameRate: 15,
         repeat: -1
     });
 
     this.anims.create({
         key: 'backward',
-        frames: this.anims.generateFrameNumbers('sprite', {start: 0, end: 2}),
-        frameRate: 20,
+        frames: this.anims.generateFrameNumbers('sprite', { start: 0, end: 3 }),
+        frameRate: 15,
         repeat: -1
     });
 
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('sprite', {start: 3, end: 5}),
-        frameRate: 20,
+        frames: this.anims.generateFrameNumbers('sprite', { start: 8, end: 11 }),
+        frameRate: 15,
         repeat: -1
     });
 
     this.anims.create({
         key: 'stop',
-        frames: [{key: 'sprite', frame: 1}],
-        frameRate: 20
+        frames: [ { key: 'sprite', frame: 12 } ],
+        frameRate: 10
     });
 
     // Check to see if player collides with enemy or sideline
     this.physics.add.overlap(player, sideline, outOfBounds, null, this);
 
     //this.physics.add.collider(player, enemy, hitEnemy, null, this);
-    this.physics.add.collider(player, enemies, hitEnemy, null, this);
+    this.physics.add.collider(player, enemies1, hitEnemy, null, this);
+    this.physics.add.collider(player, enemies2, hitEnemy, null, this);
+    this.physics.add.collider(player, enemies3, hitEnemy, null, this);
+    this.physics.add.collider(player, enemies4, hitEnemy, null, this);
 
-    text = this.add.text(0, 0).setScrollFactor(0).setFontSize(32).setColor('#ffffff');
+};
 
-}
-
-function update() {
-    if (gameOver) {
+function update ()
+{
+   if (gameOver)
+    {
         this.physics.pause();
         player.setTint(0xff0000);
 
@@ -152,72 +206,89 @@ function update() {
         // Game Over Screen
         var camx = player.x; // Location of player
         var camy = player.y;
-        var text = this.add.text(camx - 105, camy + 20, 'Game Over!', {
-            fontSize: '40px',
-            fill: '#000',
-            align: 'center'
-        });
+        var text = this.add.text(camx - 105, camy + 20, 'Game Over!', { fontSize: '40px', fill: '#000', align: 'center' });
         //loseText = this.add.text(300, 200, 'Game Over!', { fontSize: '40px', fill: '#000' });
         //endScore = this.add.text(240, 250, 'Your Score was ' + score, { fontSize: '32px', fill: '#000' });
         //highScore = this.add.text(270, 300, 'High Score: ' + localStorage.getItem("highScore"),  { fontSize: '32px', fill: '#000' });
-        newGame = this.add.text(camx - 60, camy + 60, 'Restart', {fontSize: '32px', fill: '#000'})
+        newGame = this.add.text(camx - 60, camy + 60, 'Restart', { fontSize: '32px', fill: '#000' })
             .setInteractive()
-            .on('pointerdown', function () {
+            .on('pointerdown', function(){
                 location.reload();
             });
-    }
+    };
 
-    if (touchdown) {
-        this.physics.pause();
+    if (touchdown)
+    {
+
+        score += 100;
+        scoreText.setText("Score: " + score);
+
+        player.setPosition(0, 175);
+        enemies1.setXY(-475, -200, 95);
+        enemies2.setXY(-475, -500, 95);
+        enemies3.setXY(-475, -800, 95);
+        enemies4.setXY(-475, -1000, 95);
+
+        touchdown = false;
 
         var camx = player.x; // Location of player
         var camy = player.y;
-        var text = this.add.text(camx - 105, camy + 20, 'Touchdown!', {
-            fontSize: '40px',
-            fill: '#000',
-            align: 'center'
-        });
-        newGame = this.add.text(camx - 80, camy + 55, 'Next Level', {fontSize: '32px', fill: '#000'})
-            .setInteractive()
-            .on('pointerdown', function () {
-                location.reload();
-            });
+        var text = this.add.text(camx - 85, camy + 20, 'Touchdown!', { fontSize: '30px', fill: '#000', align: 'center' });
+        // newGame = this.add.text(camx - 80, camy + 55, 'Score:' + score, { fontSize: '32px', fill: '#000' })
+        //     .setInteractive()
+        //     .on('pointerdown', function(){
+        //         this.game.restart();
+        //     });
+
+        // this.time.events.add(3000, this.text.destroy, this.text);
+
+
     }
 
     var cam = this.cameras.main;
 
     // Input Controls and Speed of Player
-    if (cursors.left.isDown) {
+    if (cursors.left.isDown)
+    {
         player.setVelocityX(-100);
 
         player.anims.play('left', true);
-    } else if (cursors.right.isDown) {
+    }
+    else if (cursors.right.isDown)
+    {
         player.setVelocityX(100);
 
         player.anims.play('right', true);
-    } else {
+    }
+    else
+    {
         player.setVelocityX(0);
     }
 
-    if (cursors.up.isDown) {
+    if (cursors.up.isDown)
+    {
         player.setVelocityY(-100);
 
         player.anims.play('forward', true);
-    } else if (cursors.down.isDown) {
+    }
+    else if (cursors.down.isDown)
+    {
         player.setVelocityY(100);
 
         player.anims.play('backward', true);
-    } else {
+    }
+    else
+    {
         player.setVelocityY(0);
 
         player.anims.play('stop');
     }
-    /*
-        text.setText([
-            'MidX: ' + player.x,
-            'MidY: ' + player.y
-        ]);
-    */
+/*
+    text.setText([
+        'MidX: ' + player.x,
+        'MidY: ' + player.y
+    ]);
+*/
     // Find Players Coordinates
     var xCord = player.x;
     var yCord = player.y;
@@ -225,17 +296,21 @@ function update() {
     // If player enters EndZone, call scoreTD() function
     if (yCord < -880) {
         scoreTD();
+        player.setX = 0;
+        player.sety = 175;
     }
 
     // If Player goes out of bounds, call outOfBounds() function
     if (xCord > 478) {
         outOfBounds();
-    } else if (xCord < -478) {
+    }
+    else if (xCord < -478) {
         outOfBounds();
     }
-}
+};
 
-function scoreTD() {
+function scoreTD ()
+{
     // Pause Game Physics
     // Touchdown Text Appears
     // Add Score
@@ -243,17 +318,21 @@ function scoreTD() {
     touchdown = true;
 }
 
-function hitEnemy(player, enemy) {
+function hitEnemy (player, enemy)
+{
     gameOver = true;
 }
 
-function outOfBounds() {
+function outOfBounds ()
+{
     gameOver = true;
 }
 
-function saveHighScore(score) {
+function saveHighScore (score)
+{
     let hs = localStorage.getItem("highScore");
-    if (score > hs) {
-        localStorage.setItem('highScore', score);
+    if(score > hs)
+    {
+    localStorage.setItem('highScore', score);
     }
 }
